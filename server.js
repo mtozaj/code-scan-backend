140a507828d3988d75cf406e15f42559dbd7c405
Duy <duynguyenquykhanh@gmail.com>
2022-11-19 15:29:26 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const bodyParse = require("body-parser");
+
+//TEST
+app.use(cors());
+app.use(bodyParse.urlencoded({ extended: true }));
+app.use(bodyParse.json());
+app.use(morgan("dev"));
+
+// main endpoint
+app.get("/compile", async (req, res) => {
+  res.status(200).json({ output: "Hello" });
+});
+
+app.get("/image/convert", async (req, res) => {
+  res.status(200).json({ text: "Hello" });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Running Server at " + port));
