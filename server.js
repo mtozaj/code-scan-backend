3b577db673ccc67531b8e8e5123c537c22490d39
Duy <duynguyenquykhanh@gmail.com>
2022-11-19 20:15:41 -0800

    test compile

--- server.js
+++ server.js
@@ -22,40 +22,18 @@ app.use(bodyParse.urlencoded({ extended: false }));
 app.use(bodyParse.json());
 app.use(morgan("dev"));
 
-// main endpoint
-app.get("/compile", async (req, res) => {
-  var request = require("request");
+// 0 = C++, 1 = C, 2 = Javascript (nodejs)
+function getLanguage(languageID) {
+  if (languageID == 0) {
+    return "cpp";
+  } else if (languageID == 1) {
+    return "c";
+  } else {
+    return "nodejs";
+  }
+}
 
-  var program = {
-    script: 'console.log("Joshua big boi")',
-    language: "python",
-    versionIndex: "4",
-    clientId: "d0c25492b573abf6ac0f5999d0f62d98",
-    clientSecret:
-      "f9416cae80b7afda1888d70f09a18d8f2b8672526d9486fd56d22096c6047367",
-  };
-  request(
-    {
-      url: "https://api.jdoodle.com/v1/execute",
-      method: "POST",
-      json: program,
-    },
-    function (error, response, body) {
-      console.log("error:", error);
-      console.log("statusCode:", response && response.statusCode);
-      console.log("body:", body);
-    }
-  );
-  res.status(200).json({ output: "YI LONG M (The real Elon)" });
-});
-
-app.get("/image/convert", async (req, res) => {
-  const [result] = await client.documentTextDetection("foo.png");
-  const fullTextAnnotation = result.fullTextAnnotation;
-  console.log(fullTextAnnotation.text);
-  res.status(200).json({ text: fullTextAnnotation.text });
-});
-
+// upload image
 app.post("/image/upload", async (req, res, next) => {
   try {
     // to declare some path to store your converted image
@@ -72,6 +50,45 @@ app.post("/image/upload", async (req, res, next) => {
   } catch (e) {
     next(e);
   }
+});
+
+app.get("/code_recognition", async (req, res) => {
+  try {
+    const [result] = await client.documentTextDetection("foo.png");
+    const fullTextAnnotation = result.fullTextAnnotation;
+    res.status(200).json({ output: fullTextAnnotation.text });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+// Compile code
+app.get("/compile", async (req, res) => {
+  var request = require("request");
+  const { languageID } = req.body;
+
+  // 0 = C++ (versionIndex 4), 1 = C, 2 = Javascript (nodejs)
+  var program = {
+    script: "console.log(1 + 1)",
+    language: getLanguage(languageID),
+    versionIndex: "4",
+    clientId: "d0c25492b573abf6ac0f5999d0f62d98",
+    clientSecret:
+      "f9416cae80b7afda1888d70f09a18d8f2b8672526d9486fd56d22096c6047367",
+  };
+  request(
+    {
+      url: "https://api.jdoodle.com/v1/execute",
+      method: "POST",
+      json: program,
+    },
+    function (error, response, body) {
+      console.log("error:", error);
+      console.log("statusCode:", response && response.statusCode);
+      console.log("body:", body);
+      res.status(200).json({ output: body.output });
+    }
+  );
 });
 
 app.get("/", async (req, res) => {
