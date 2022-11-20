1cdd3386e916895fa78824f7e3faa2daf30bc472
bob <bob@>
2022-11-20 02:09:25 -0800

    update BE

--- server.js
+++ server.js
@@ -16,11 +16,10 @@ const client = new vision.ImageAnnotatorClient({
   keyFilename: "key.json",
 });
 
-
 //TEST
 app.use(cors());
 app.use(bodyParse.urlencoded({ extended: false }));
-app.use(bodyParse.json({limit: '50mb'}));
+app.use(bodyParse.json());
 app.use(morgan("dev"));
 
 // 0 = C++, 1 = C, 2 = Javascript (nodejs)
@@ -53,43 +52,37 @@ app.post("/image/upload", async (req, res, next) => {
   }
 });
 
-app.get("/code_recognition", async (req, res) => {
-  try {
-    const [result] = await client.documentTextDetection("foo.png");
-    const fullTextAnnotation = result.fullTextAnnotation;
-    res.status(200).json({ output: fullTextAnnotation.text });
-  } catch (e) {
-    console.log(e);
-  }
-});
-
 // Compile code
 app.post("/compile", async (req, res) => {
   var request = require("request");
   const { languageID } = req.body;
-
+  try {
+    const [result] = await client.documentTextDetection("./images/foo.png");
+    const fullTextAnnotation = result.fullTextAnnotation;
+    console.log("text: " + fullTextAnnotation.text);
+    var program = {
+      script: fullTextAnnotation.text,
+      language: getLanguage(languageID),
+      versionIndex: "4",
+      clientId: "d0c25492b573abf6ac0f5999d0f62d98",
+      clientSecret:
+        "f9416cae80b7afda1888d70f09a18d8f2b8672526d9486fd56d22096c6047367",
+    };
+    request(
+      {
+        url: "https://api.jdoodle.com/v1/execute",
+        method: "POST",
+        json: program,
+      },
+      function (error, response, body) {
+        console.log(body);
+        res.status(200).json({ output: body.output });
+      }
+    );
+  } catch (e) {
+    console.log(e);
+  }
   // 0 = C++ (versionIndex 4), 1 = C, 2 = Javascript (nodejs)
-  var program = {
-    script: "console.log(1 + 1)",
-    language: getLanguage(languageID),
-    versionIndex: "4",
-    clientId: "d0c25492b573abf6ac0f5999d0f62d98",
-    clientSecret:
-      "f9416cae80b7afda1888d70f09a18d8f2b8672526d9486fd56d22096c6047367",
-  };
-  request(
-    {
-      url: "https://api.jdoodle.com/v1/execute",
-      method: "POST",
-      json: program,
-    },
-    function (error, response, body) {
-      console.log("error:", error);
-      console.log("statusCode:", response && response.statusCode);
-      console.log("body:", body);
-      res.status(200).json({ output: body.output });
-    }
-  );
 });
 
 app.get("/", async (req, res) => {
