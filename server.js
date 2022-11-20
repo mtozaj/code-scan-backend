705e8dacd894f8b13350cc27f70f3019d975f286
Ernest Lu <elu.jcr7@gmail.com>
2022-11-19 16:42:55 -0800

    Google Vision API added

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const morgan = require("morgan");
 const app = express();
 const bodyParse = require("body-parser");
 
+const vision = require('@google-cloud/vision');
+
+// Creates a client
+const client = new vision.ImageAnnotatorClient({
+  projectId: "caramel-duality-369122",
+  keyFilename: "key.json"
+});
+
 //TEST
 app.use(cors());
 app.use(bodyParse.urlencoded({ extended: true }));
@@ -17,7 +25,11 @@ app.get("/compile", async (req, res) => {
 });
 
 app.get("/image/convert", async (req, res) => {
-  res.status(200).json({ text: "Let's win the hackathon!!" });
+  const [result] = await client.documentTextDetection("drake.jpg");
+const fullTextAnnotation = result.fullTextAnnotation;
+console.log(fullTextAnnotation.text);
+
+  res.status(200).json({ text: fullTextAnnotation.text });
 });
 
 const port = process.env.PORT || 5000;
